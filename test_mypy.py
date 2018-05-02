86f79c693d46696dad0d91eb2a11b432e1b3ec4f
Jesse Vogt <jesse.vogt@gmail.com>
2018-05-01 22:59:26 -0500

    added tests ci

--- test_mypy.py
+++ test_mypy.py
@@ -5,10 +5,24 @@ from myp import get_ip, InconsistentIPAddressesFound
 from services import TextService, JSONService
 
 
-# def test_myp_get_ip_text(aresponses: ResponsesMockServer, event_loop):
+def test_myp_get_ip_text(aresponses: ResponsesMockServer, event_loop):
+    aresponses.add('text.com', '/', 'GET', '1.2.3.4')
+    assert get_ip([TextService('http://text.com')], loop=event_loop) == '1.2.3.4'
 
 
-# def test_myp_get_ip_json(aresponses: ResponsesMockServer, event_loop):
+def test_myp_get_ip_json(aresponses: ResponsesMockServer, event_loop):
+    aresponses.add('json.com', '/', 'GET',
+                   Response(body=b'{"ip": "1.2.3.5"}',
+                            headers={'content-type': 'application/json'}))
+    assert get_ip([JSONService('http://json.com', 'ip')], loop=event_loop) == '1.2.3.5'
 
 
-# def test_myp_bad_ip(aresponses: ResponsesMockServer, event_loop):
+def test_myp_bad_ip(aresponses: ResponsesMockServer, event_loop):
+    aresponses.add('bad.com', '/', 'GET', '1.2.3.6')
+    aresponses.add('good.com', '/', 'GET', '1.2.3.7')
+
+    with pytest.raises(InconsistentIPAddressesFound):
+        assert get_ip([
+            TextService('http://bad.com'),
+            TextService('http://good.com'),
+        ], loop=event_loop) == '1.2.3.4'
